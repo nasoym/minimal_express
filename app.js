a02ffdb3c18a62e3dd0a75f364d2471c51d932f2
Sinan Goo <sinan.goo@hypoport.de>
2016-12-12 14:41:50 +0100

    include express body parser

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.text());
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send("hello world\n");
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.post('/json', bodyParser.json(), function (req, res) {
+  console.log("body: ", req.body);
+  res.send("hello world\n");
 })
+
+ app.post('/raw', bodyParser.raw(), function (req, res) {
+  console.log("body: " + req.body.toString());
+  res.send("hello world\n");
+})
+
+app.post('/text', bodyParser.text({type:"*/*"}), function (req, res) {
+  var body = req.body;
+  var firstLine = body.split('\n')[0];
+  var secondLine = body.split('\n')[1];
+  var response = "first: " + firstLine;
+  response += "\n";
+  response += "second: " + secondLine;
+  response += "\n";
+  res.send(response);
+})
+
+app.listen(8080)
